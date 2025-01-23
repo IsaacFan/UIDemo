b063757873239ad30df26511ce8cf51f19fdeaa9
Isaac Chen <31337168+IsaacFan@users.noreply.github.com>
2025-01-23 22:23:20 +0000

    Add files via upload

---
+++ Source/Private/Game/DMGameUserSettingConstants.h
@@ -0,0 +1,31 @@
+// Copyright CL Chen - All Rights Reserved.
+
+#pragma once
+
+namespace DMGameUserSettingConstants
+{
+	static constexpr float DefaultMouseSensitivity{ 0.045f };
+	static constexpr float MinMouseSensitivity{ 0.001f };
+	static constexpr float MaxMouseSensitivity{ 0.1f };
+
+	static constexpr float DefaultMouseADSSensitivityModifier{ 1.0f };
+	static constexpr float MinMouseADSSensitivityModifier{ 0.1f };
+	static constexpr float MaxMouseADSSensitivityModifier{ 2.0f };
+
+	static constexpr float DefaultGamepadSensitivityX{ 3.6f };
+	static constexpr float MinGamepadSensitivityX{ 0.1f };
+	static constexpr float MaxGamepadSensitivityX{ 8.0f };
+
+	static constexpr float DefaultGamepadSensitivityY{ 2.7f };
+	static constexpr float MinGamepadSensitivityY{ 0.1f };
+	static constexpr float MaxGamepadSensitivityY{ 8.0f };
+
+	static constexpr float DefaultGamepadADSSensitivityModifierX{ 0.7f };
+	static constexpr float MinGamepadADSSensitivityModifierX{ 0.1f };
+	static constexpr float MaxGamepadADSSensitivityModifierX{ 2.0f };
+
+	static constexpr float DefaultGamepadADSSensitivityModifierY{ 0.6f };
+	static constexpr float MinGamepadADSSensitivityModifierY{ 0.1f };
+	static constexpr float MaxGamepadADSSensitivityModifierY{ 2.0f };
+
+}
